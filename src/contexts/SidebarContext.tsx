ed522abd143e7aab03e0ca131bde78e287eafbc1
Darnley Costa <darnleysilva@gmail.com>
2020-04-05 18:48:21 -0300

    Adicionado o SidebarContext

---
+++ src/contexts/SidebarContext.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+/**
+ * The SidebarContext interface with just the setSidebarComponent function.
+ */
+export interface ISidebarChangerContext {
+  /**
+   * Set the sidebar component.
+   * If it is null, the sidebar will disappear.
+   * @field
+   */
+  setSidebarComponent: (newComponent: any) => void;
+}
+
+/**
+ * The SidebarContext interface with the setSidebarComponent and sidebarComponent.
+ *
+ * @extends {ISidebarChangerContext}
+ */
+export interface ISidebarContext extends ISidebarChangerContext {
+  /**
+   * The sidebar component.
+   */
+  sidebarComponent: any;
+}
+
+/**
+ * The sidebar context for component injection.
+ */
+const SidebarContext = React.createContext<ISidebarContext>({
+  sidebarComponent: <span></span>,
+  setSidebarComponent: newComponent => {},
+});
+
+export default SidebarContext;
