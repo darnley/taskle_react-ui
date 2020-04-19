4882cfc18ce988059ba434edb6d7999f75c9318d
Darnley Costa <darnleysilva@gmail.com>
2020-04-19 15:01:39 -0300

    Criados os componentes básicos da organização/Pessoas

---
+++ src/components/organization/People/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface IPeopleProps {}
+
+const People: React.FunctionComponent<IPeopleProps> = props => {
+  return <div></div>;
+};
+
+export default People;
---
+++ src/components/organization/Teams/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface ITeamsProps {}
+
+const Teams: React.FunctionComponent<ITeamsProps> = props => {
+  return <div></div>;
+};
+
+export default Teams;
