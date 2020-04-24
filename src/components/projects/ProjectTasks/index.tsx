c1fed3f3f6556c05a364920e5eadf6b54b0f4827
Darnley Costa <darnleysilva@gmail.com>
2020-04-23 21:02:56 -0300

    Corrigido o flex box e adicionado o badge como keywords

--- src/components/projects/ProjectTasks/index.tsx
+++ src/components/projects/ProjectTasks/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Row, Button, Card, Col } from 'react-bootstrap';
+import { Container, Row, Button, Card, Col, Badge } from 'react-bootstrap';
 import { ITask } from '../../../interfaces/ITask';
 import { getAllTasks } from '../../../services/project/task';
 import TaskStatusIcon from '../../icons/TaskStatusIcon';
@@ -28,18 +28,41 @@ const ProjectTasks: React.FunctionComponent<IProjectTasksProps> = props => {
           <Card className="w-100 mt-2" key={task._id}>
             <Card.Body>
               <Row>
-                <Col sm={1} className="text-center">
+                <Col
+                  sm={1}
+                  className="d-flex align-items-center justify-content-center"
+                >
                   <TaskStatusIcon status={task.status} width="30em" />
                 </Col>
-                <Col md={8}>
-                  <TaskComplexityIcon
-                    complexity={task.complexity}
-                    width="30em"
-                  />
-                  {task.description}
-                  {task.complexity}
+                <Col md={8} className="project-task-middle-data">
+                  <Row className="project-task-top">
+                    <TaskComplexityIcon
+                      complexity={task.complexity}
+                      width="30em"
+                    />
+                  </Row>
+                  <Row className="project-task-description">
+                    {task.description}
+                  </Row>
+                  <Row className="project-task-keywords">
+                    {task.keywords.map((keyword, index) => (
+                      <Badge
+                        key={`task-${task._id}-keyword-${index}`}
+                        variant="secondary"
+                        className="mr-1"
+                      >
+                        {keyword}
+                      </Badge>
+                    ))}
+                  </Row>
+                  <Row className="proejct-task-footer">footer</Row>
                 </Col>
-                <Col md={3}>show</Col>
+                <Col
+                  md={3}
+                  className="d-flex align-items-center justify-content-center"
+                >
+                  show
+                </Col>
               </Row>
             </Card.Body>
           </Card>
