281c5bf27652ffef93792eb4bd20f6552e5ac079
Darnley Costa <darnleysilva@gmail.com>
2020-05-25 18:34:34 -0300

    Adicionado o serviço para encerrar projetos

--- src/services/project/index.ts
+++ src/services/project/index.ts
@@ -1,6 +1,7 @@
 import IProject from '../../interfaces/IProject';
 import authenticatedAxios from '../authenticatedAxios';
 import IMilestoneApi from '../../interfaces/IMilestoneApi';
+import IProjectEndRating from '../../interfaces/IProjectEndRating';
 
 export function getProject(projectId: string): Promise<IProject> {
   return new Promise<IProject>((resolve, reject) => {
@@ -36,6 +37,26 @@ export function getAllMilestones(projectId: string): Promise<IMilestoneApi[]> {
   return new Promise<IMilestoneApi[]>((resolve, reject) => {
     authenticatedAxios
       .get<IMilestoneApi[]>(`/projects/${projectId}/milestones`)
+      .then(res => resolve(res.data))
+      .catch(err => reject(err));
+  });
+}
+
+export function endProject(
+  projectId: string,
+  ratings: IProjectEndRating[]
+): Promise<IProjectEndRating[]> {
+  if (!projectId) {
+    throw new Error("Argument 'projectId' is required when ending a project.");
+  }
+
+  if (!ratings) {
+    throw new Error("Argument 'ratings' is required when ending a project.");
+  }
+
+  return new Promise<IProjectEndRating[]>((resolve, reject) => {
+    authenticatedAxios
+      .post<IProjectEndRating[]>(`/projects/${projectId}/end`, ratings)
       .then(res => resolve(res.data))
       .catch(err => reject(err));
   });
