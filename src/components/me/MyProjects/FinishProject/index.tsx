87a65f3150cc437b23aef270f1741a52d207234c
Darnley Costa <darnleysilva@gmail.com>
2020-05-31 16:02:34 -0300

    Adicionados detalhes da página de encerramento de projeot
    Ainda nao possui a funcionalidade completamente implementada.

--- src/components/me/MyProjects/FinishProject/index.tsx
+++ src/components/me/MyProjects/FinishProject/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import IProject from '../../../../interfaces/IProject';
+import { getProject } from '../../../../services/project';
+import { Button, Form } from 'react-bootstrap';
 
 export interface IFinishProjectProps {
   projectId: string;
 }
 
 const FinishProject: React.FunctionComponent<IFinishProjectProps> = props => {
-  return <span>{props.projectId}</span>;
+  const [project, setProject] = useState<IProject>();
+
+  useEffect(() => {
+    getProject(props.projectId)
+      .then(p => {
+        setProject(p);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, [props.projectId]);
+
+  return (
+    <Form>
+      <fieldset>
+        <p className="text-muted">
+          Dê estrelas para os que trabalharam bem no projeto.
+        </p>
+      </fieldset>
+      <Button type="submit" variant="primary" block>
+        Enviar
+      </Button>
+    </Form>
+  );
 };
 
 export default FinishProject;
