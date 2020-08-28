5ebcd9b0f571c905230e617b1c13ef30b4026aa7
Darnley Costa <darnleysilva@gmail.com>
2020-08-28 17:24:41 -0300

    Adiciona a quantidade de pessoas por time

--- src/components/organization/Teams/TeamsDeck/TeamsItem/index.tsx
+++ src/components/organization/Teams/TeamsDeck/TeamsItem/index.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ITeam from '../../../../../interfaces/ITeam';
 import { Card, Row, Col } from 'react-bootstrap';
+import { IUser } from '../../../../../interfaces/IUser';
+import { getTeamPeople } from '../../../../../services/team';
+import Skeleton from 'react-loading-skeleton';
 
 export interface ITeamsItemProps {
   team: ITeam;
 }
 
 const TeamsItem: React.FunctionComponent<ITeamsItemProps> = props => {
+  const [people, setPeople] = useState<IUser[]>([]);
+  const [isPeopleDataReady, setIsPeopleDataReady] = useState<boolean>(false);
+
+  const getData = (teamId: string) => {
+    getTeamPeople(teamId)
+      .then(res => {
+        setPeople(res);
+        setIsPeopleDataReady(true);
+      })
+      .catch(console.error);
+  };
+
+  useEffect(() => {
+    getData(props.team._id);
+  }, [props.team._id]);
+
   return (
     <Card>
       <Card.Body>
         <Row>
-          <Col md={5}>{props.team.name}</Col>
+          <Col md={5}>
+            <div>{props.team.name}</div>
+            <div>
+              <small className="text-muted">
+                {!isPeopleDataReady && <Skeleton width={50} />}
+                {isPeopleDataReady && `${people.length} pessoa(s)`}
+              </small>
+            </div>
+          </Col>
           <Col md={5}></Col>
           <Col md={2}>
             <div className="view-profile">
