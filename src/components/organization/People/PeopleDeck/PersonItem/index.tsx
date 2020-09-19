9cefac63d14aa5b291edb1429d89239bef220e62
Darnley Costa <darnleysilva@gmail.com>
2020-09-18 22:42:39 -0300

    Deixa todos os cards de pessoas do mesmo tamanho

--- src/components/organization/People/PeopleDeck/PersonItem/index.tsx
+++ src/components/organization/People/PeopleDeck/PersonItem/index.tsx
@@ -20,39 +20,41 @@ const PersonItem: React.FunctionComponent<IPersonItemProps> = props => {
   };
 
   return (
-    <Card>
-      <Card.Body>
-        <Row className="h-100">
-          <Col md={5}>
-            <div className="person-name font-weight-bold">
-              {props.person.firstName + ' ' + props.person.lastName}
-              <a
-                href={`mailto:${props.person.emailAddress}`}
-                rel="noopener noreferrer"
-                target="_blank"
-                className="person-name-mail"
+    <Col md={4} className="pl-0 pr-0">
+      <Card className="h-100">
+        <Card.Body>
+          <Row className="h-100">
+            <Col md={5}>
+              <div className="person-name font-weight-bold">
+                {props.person.firstName + ' ' + props.person.lastName}
+                <a
+                  href={`mailto:${props.person.emailAddress}`}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="person-name-mail"
+                >
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </a>
+              </div>
+              <small className="person-position text-muted font-italic">
+                {props.person.position ?? 'Sem cargo'}
+              </small>
+            </Col>
+            <Col md={5}></Col>
+            <Col md={2}>
+              <Button
+                variant="primary"
+                className="h-100 float-right ml-1"
+                title="Ver o projeto"
+                onClick={() => handleSeePersonClick(props.person)}
               >
-                <FontAwesomeIcon icon={faEnvelope} />
-              </a>
-            </div>
-            <small className="person-position text-muted font-italic">
-              {props.person.position ?? 'Sem cargo'}
-            </small>
-          </Col>
-          <Col md={5}></Col>
-          <Col md={2}>
-            <Button
-              variant="primary"
-              className="h-100 float-right ml-1"
-              title="Ver o projeto"
-              onClick={() => handleSeePersonClick(props.person)}
-            >
-              <FontAwesomeIcon icon={faEye} />
-            </Button>
-          </Col>
-        </Row>
-      </Card.Body>
-    </Card>
+                <FontAwesomeIcon icon={faEye} />
+              </Button>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
--- src/components/organization/People/PeopleDeck/index.tsx
+++ src/components/organization/People/PeopleDeck/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IUser } from '../../../../interfaces/IUser';
 import PersonItem from './PersonItem';
-import { CardDeck } from 'react-bootstrap';
+import { CardDeck, Col } from 'react-bootstrap';
 import chunkArray from '../../../../utils/chunkArray';
 
 export interface IPeopleDeck {
@@ -13,6 +13,7 @@ const PeopleDeck: React.FunctionComponent<IPeopleDeck> = props => {
     <>
       {props.people &&
         chunkArray(props.people).map((people, index) => (
+
           <CardDeck className="mb-2" key={index}>
             {people.map((person, index, array) => (
               <PersonItem person={person} key={person._id} />
