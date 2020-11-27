537602fd6549e53e171e445ca53113d2841daae8
Darnley Costa <darnley@outlook.com>
2020-11-27 20:35:07 -0300

    Adiciona alteração de isActive e role quando é super usuario

--- src/components/organization/People/AddPerson/index.tsx
+++ src/components/organization/People/AddPerson/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import IFormDataAddPerson from '../../../../interfaces/forms/IFormDataAddPerson';
-import { Form, Button, Alert, Badge } from 'react-bootstrap';
+import { Form, Button, Alert, Badge, Card } from 'react-bootstrap';
 import classNames from 'classnames';
 import RBRef from '../../../../types/RBRef';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -13,6 +13,7 @@ import { addPerson, getPerson, updatePerson } from '../../../../services/people'
 import Role from '../../../../enums/Role';
 import { useToasts } from 'react-toast-notifications';
 import { IUser } from '../../../../interfaces/IUser';
+import getAuthenticatedUser from '../../../../services/user/getAuthenticatedUser';
 
 export interface IAddPersonProps {
   personId?: string;
@@ -27,6 +28,7 @@ const AddPerson: React.FunctionComponent<IAddPersonProps> = props => {
   const { addToast } = useToasts();
   const [addPersonErrorMessage, setAddPersonErrorMessage] = useState<string>();
   const [currentPerson, setCurrentPerson] = useState<IUser>();
+  const [currentLoggedUser, setCurrentLoggedUser] = useState<IUser>();
 
   const getCurrentPersonData = (personId: string) => {
     getPerson(personId)
@@ -38,6 +40,9 @@ const AddPerson: React.FunctionComponent<IAddPersonProps> = props => {
     getAllTeams().then(res => {
       setTeams(res);
     });
+
+    getAuthenticatedUser()
+      .then(setCurrentLoggedUser);
 
     if (props.personId) {
       getCurrentPersonData(props.personId);
@@ -237,6 +242,46 @@ const AddPerson: React.FunctionComponent<IAddPersonProps> = props => {
           )}
         ></Typeahead>
       </Form.Group>
+      {props.personId && currentLoggedUser?.role === Role.Super &&
+        <Card className="mb-3">
+          <Card.Header className="p-2">
+            Segurança
+          </Card.Header>
+          <Card.Body className="p-2">
+            <Form.Group controlId="role">
+              <Form.Control
+                name="role"
+                as="select"
+                className={classNames({
+                  'is-invalid': errors.firstName,
+                })}
+                ref={
+                  register({
+                    required: true,
+                  }) as RBRef
+                }>
+                <option value={Role.Normal} selected={currentPerson?.role === Role.Normal ? true : false}>Usuário normal</option>
+                <option value={Role.Super} selected={currentPerson?.role === Role.Super ? true : false}>Super usuário</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="isActive" className="mb-2">
+              <Form.Check
+                name="isActive"
+                type="checkbox"
+                label="Está ativo?"
+                defaultChecked={currentPerson?.isActive}
+                className={classNames({
+                  'is-invalid': errors.firstName,
+                  'ml-1': true
+                })}
+                ref={
+                  register({
+                    required: false,
+                  }) as RBRef
+                } />
+            </Form.Group>
+          </Card.Body>
+        </Card>}
       <Alert variant="info">
         A senha de acesso será enviada para o <strong>e-mail cadastrado</strong>
       </Alert>
